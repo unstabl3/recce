a5291676ae80dcf3bb3b1af86f8c00cba21df575
unstabl3 <48474764+unstabl3@users.noreply.github.com>
2019-08-29 15:51:28 +0530

    second release

--- recce.py
+++ recce.py
@@ -3,12 +3,17 @@
 import os
 import sys
 import platform
-import time
+import requests
+import pycurl
+import re
+import ssl
+import warnings
 
 ver = platform.python_version()
+recce_version = '2.1'
 
 if (ver <= '3'):
-        print("\033[91m Breakfold isn't compatible with python2 use python 3.x\033[00m")
+        print("\033[91m Recce isn't compatible with python2 use python 3.x\033[00m")
         sys.exit(1)
 
 import argparse
@@ -18,25 +23,43 @@ parser = argparse.ArgumentParser(description="""\033[93m[~] Domain status checke
 									https://twitter.com/chaskar_shubham\033[00m""")
 
 group = parser.add_mutually_exclusive_group()
+
+group1 = parser.add_mutually_exclusive_group()
+
+group2 = parser.add_mutually_exclusive_group()
 												#Taking arguments from CLI
 parser.add_argument("-v","--verbose", help="verbose",action="store_true")
 
-parser.add_argument("-o","--output", help="write active domains in new file" ,metavar='out-file')
+group1.add_argument("-o","--output", help="write active domains in new file" ,metavar="out-file")
+
+group1.add_argument("-c","--csv", help="mention .csv file to write output",metavar="csv-file")
 
 parser.add_argument("-t","--threads", help="number of concurrent threads" ,type=int,metavar="Threads")
 
 group.add_argument("-f","--file", help="File which consist domains(sub.example.com)",metavar="Input file")
 
-group.add_argument("-u","--url", help="single domain check",metavar="URL")
+group.add_argument("-d","--domain", help="single domain check",metavar="domain")
+
+group2.add_argument("-s","--server",help="print web server",action="store_true")
+
+parser.add_argument("-u","--update",help="update recce",action="store_true")
+
+group2.add_argument("-l","--live",help="only print live subdomains",action="store_true")
+
+parser.add_argument("-r","--length",help="print response length",action="store_true")
 
 args = parser.parse_args()
 
 verbose = args.verbose
 file = args.file
-url = args.url
+domain_name = args.domain
 output = args.output
 threads = args.threads
-
+server = args.server
+live = args.live
+csv = args.csv
+length = args.length
+update = args.update
 										#Just A fancy banner!
 print("""\033[91m
 .______       _______   ______   ______  _______
@@ -49,42 +72,231 @@ print("""\033[91m
 					\033[93m v1.0 By shubham_chaskar\033[00m
 """)
 
+warnings.filterwarnings('ignore')
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+if not threads:										#default number of threads
+	threads = 20
+if csv:
+	if not csv.endswith(".csv"):
+		print("\033[91m[!]Your file must end with .csv extension\033[00m")
+		sys.exit(1)
+try:
+	from io import BytesIO
+except ImportError:
+	from StringIO import StringIO as BytesIO
+
+
+def updater():
+
+	res = requests.get("https://raw.githubusercontent.com/unstabl3/recce/master/recce.py").text
+
+	r2 = re.search(r"recce_version = '(.*)'",res)
+
+	new_match = str(r2.group())
+
+	new_version = re.search(r"\d+\.\d+", new_match).group()
+
+	if new_version > recce_version:
+
+		print("\033[93m[~]Update available for recce and Download is in progress.\033[00m")
+
+		current_directory = os.getcwd()
+
+		new_folder = curren_directory.split("/")[-1]
+
+		os.system('git clone --quiet https://github.com/unstabl3/recce.git %s' % (new_folder))
+
+		os.system('cp -r %s/%s/* %s' % (current_directory,new_folder,current_directory))
+
+		os.system('rm -rf %s/%s/' % (current_directory,new_folder))
+	else:
+		print("\033[91m[!]Recce is already in his latest version\033[00m")
+
+		sys.exit(0)
+
+if update:
+	updater()
+
+headers = {}
+redirect = []
+csv_list = []
 
 if verbose:
 	print("\033[93m[~] Verbosity is enabled..\033[00m")
 
-if not threads:										#default number of threads
-	threads = 20
 
-t = time.time()
+def header_function(header_line):
+
+	header_line = header_line.decode('iso-8859-1')
+
+	if ':' not in header_line:
+		return
+
+	name, value = header_line.split(':', 1)
+
+	name = name.strip()
+
+	value = value.strip()
+
+	name = name.lower()
+
+	headers[name] = value
+
+def csv_output(csv_list):
+
+	csv_file = ''
+
+	if server:
+		if length:
+			csv_file += 'Domain,Status_code,Server_name,Response_length\n'
+		else:
+			csv_file += 'Domain,Status_code,Server_name\n'
+
+	else:
+		csv_file += 'Domain,Status_code\n'
+
+	for item in csv_list:
+		csv_file += item + '\n'
+	report = csv_file
+	try:
+		with open(csv, 'a') as f:
+
+			f.write(report)
+
+		f.close()
+
+		print("\033[93m[!]File saved\033[00m")
+
+	except Exception as ex:
+		print(ex)
+
 
 def recce(domain):
-												#This function will make request to domain
-	check = "curl -I " + domain + " -s -m 15 --write-out %{http_code} --output /dev/null"
 
-	answer = os.popen(check)
+	buffer = BytesIO()
 
-	result = answer.read()
+	req = pycurl.Curl()
 
+	req.setopt(pycurl.URL, domain)
+
+	req.setopt(req.FOLLOWLOCATION, True)
+
+	req.setopt(pycurl.HEADERFUNCTION, header_function)
+
+	req.setopt(req.WRITEDATA, buffer)
+
+	try:
+		req.perform()
+	except:
+		pass
+
+	result = req.getinfo(req.RESPONSE_CODE)
+											#This function will make request to domain
+	if 'location' in headers:
+
+		url = str(headers['location'])
+
+		redirect.append(url)
+
+	if live:
+
+		if not result == 0:
+
+			print("\033[92m",domain.strip(),"\033[00m")
 	return result
 
+def server_check(domain):
+
+	buffer = BytesIO()
+
+	ser = pycurl.Curl()
+
+	ser.setopt(pycurl.URL, domain)
+
+	ser.setopt(ser.FOLLOWLOCATION, True)
+
+	ser.setopt(pycurl.HEADERFUNCTION, header_function)
+
+	ser.setopt(ser.WRITEDATA, buffer)
+
+	try:
+		ser.perform()
+	except:
+		pass
+
+	if 'server' in headers or 'content-length' in headers:
+
+		return headers['server'],headers['content-length']
 
 def check(data,domain):
 												#This function will compare result
-	if (data == "000"):
+	if (data == 0):
 
-		print("\033[91m[!] Domain " , domain[:-1] ," is Down..\033[00m")
+		print("\033[91m[~]Domain name: " , domain.strip())
+
+		print("[!]Status: \tDown\033[00m")
+
+		print("-" * 150)
 	else:
+
 		if verbose:
-			print("\033[92m[~] Domain " , domain[:-1] , " is live with status code: " , data, "\033[00m")
+
+			print("\033[92m[~]Domain name: " , domain.strip())
+
+			print("[!]Status: \t live")
+
+			print("[@]Response: \t",data)
+
+			if server:
+				server_name,c_length = server_check(domain)
+
+				print("[$]Server: \t",server_name,"\033[00m")
+
+				if length:
+
+					print("\033[92m[#]Length: \t",c_length,"\033[00m")
+
+			print("-" * 150)
+
 		else:
-			print("\033[92m[~] Domain " , domain[:-1] , " is live \033[00m")
+			if server:
+				server_name,c_length = server_check(domain)
+
+				print("\033[92m[~]Domain name: " , domain.strip())
+
+				print("[!]Status: \t live")
+
+				print("[$]Server: \t",server_name,"\033[00m")
+
+				if length:
+
+					print("\033[92m[#]Length: \t",c_length,"\033[00m")
+
+				print("-" * 150)
+
+			else:
+				print("\033[92m[~]Domain name: " , domain.strip())
+
+				print("[!]Status: \t live\033[00m")
+
+				print("-" * 150)
+
 		if output:
 			with open(output,"a") as output_file:						#Writing output to new file
 				if verbose:
 					print("\033[93m[~] Writing into file..\033[00m")
 
-				output_file.write(domain)
+				output_file.write(domain.strip())
+		if csv:
+			if server:
+				if length:
+					csv_list.append(str(domain.strip()) + "," + str(data) + "," + str(server_name) + "," + str(c_length))
+				else:
+					csv_list.append(str(domain.strip()) + "," + str(data) + "," + str(server_name))
+			else:
+				csv_list.append(str(domain.strip()) + "," + str(data))
 
 if file:
 
@@ -104,15 +316,15 @@ if file:
 
 		#Start the load operations and mark each future with its domain
 
-			futures = {pool.submit(recce,domain[:-1]):domain for domain in f}
+			futures = {pool.submit(recce,domain.strip()):domain for domain in f}
 
 			for future in concurrent.futures.as_completed(futures):
 				domain = futures[future]
 
 				try:
 					data = future.result()
-
-					check(data,domain)
+					if not live:
+						check(data,domain)
 
 				except Exception as exc:
 
@@ -122,7 +334,8 @@ if file:
 		print("\033[91m[!] File not found..\033[00m")
 		sys.exit(1)
 
-if url:													#For single domain check
+if domain_name:													#For single domain check
+
 	check = "curl -I " + url + " -s -m 15 --write-out %{http_code} --output /dev/null/"
 
 	answer = os.popen(check)
@@ -137,4 +350,27 @@ if url:													#For single domain check
 			print("\033[92m[~] Host ", url, "is live\033[00m")
 
 
-print("\033[93m[~] Total time taken: " , time.time() -t , "\033[00m")
+if not len(redirect) == 0:
+
+	print("\033[93m*" * 150)
+	print("*" * 150)
+	print("[~]Redirect urls found")
+	print("\033[93m*" * 150)
+	print("*" * 150,"\033[00m")
+
+	pool = concurrent.futures.ThreadPoolExecutor(max_workers=threads)
+
+	futures = {pool.submit(recce,domain.strip()):domain for domain in redirect}
+
+	for future in concurrent.futures.as_completed(futures):
+		domain = futures[future]
+
+		try:
+			data = future.result()
+			if not live:
+				check(data,domain)
+		except Exception as ex:
+			print(ex)
+
+if csv:
+	csv_output(csv_list)
